162c8d4f18244720653ee80b07f416af0ddcb85d
Trixie Devine <sward@Stacy-Wards-MacBook-Pro-3.local>
2017-08-08 11:04:07 -0400

    sytnax errors?

--- random_pair.rb
+++ random_pair.rb
@@ -1,7 +1,27 @@
-def student_list(student_name)
-	student_array = []
-	if student_name != ""
-		student_array.push(student_name)
+$student_array = []
+
+# def student_list(student_name)
+# 	if student_name != ""
+# 		$student_array.push(student_name)
+# 	end
+# 	$student_array
+# end
+
+
+def teacher_input_array(student name, student_number)
+	# puts $student_array
+	if student_name == nil
+		student_number.to_i
+		student_number.times do
+		p "What is student's name?"
+		teacher_input = gets.chomp
+		$student_array.push(teacher_input)
+		end
+	else 
+		$student_array.push(student_name)
 	end
-	student_array
+	$student_array.length
 end
+
+student_number = 4
+teacher_input_array(student_number)
