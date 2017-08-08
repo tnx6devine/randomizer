e1846ef42b586b21a487e72d0d28f52c96f6c837
Trixie Devine <sward@Stacy-Wards-MacBook-Pro-3.local>
2017-08-08 11:46:53 -0400

    Jeremy didn't like my changes

--- random_pair.rb
+++ random_pair.rb
@@ -1,19 +1,26 @@
-
-
-def teacher_input_array(student_name, student_number)
+def student_list(student_name)
 	student_array = []
-	if student_name == nil
-		ask = student_number.to_i
-		ask.times do
-		p "What is student's name?"
-		teacher_input = gets.chomp
-		student_array.push(teacher_input)
-		end
-	else 
+	if student_name != ""
 		student_array.push(student_name)
 	end
-	student_array.to_a
+	student_array
 end
+
+# def teacher_input_array(student_name)
+# 	student_array = []
+# 	if student_name == nil
+# 		ask = student_number.to_i
+# 		ask.times do
+# 		p "What is student's name?"
+# 		teacher_input = gets.chomp
+# 		student_array.push(teacher_input)
+# 		end
+# 	else 
+# 		student_array.push(student_name)
+# 	end
+# 	puts student_array
+# 	student_array
+# end
 
 # student_name = nil
 # student_number = 4
--- randomizer_test.rb
+++ randomizer_test.rb
@@ -1,3 +1,28 @@
+# require "minitest/autorun"
+# require_relative "random_pair.rb"
+
+# class TestRandom < Minitest::Test
+
+# 	def test_assert_that_1_equals_1
+# 		assert_equal(1,1)
+# 	end
+
+# 	def test_assert_that_there_is_an_array
+# 		student_name = nil
+# 		assert_equal([],teacher_input_array(student_name)
+# 	end
+
+# 	def test_assert_that_student_array_returns_correct_value
+# 		student_name = "jeremy"
+# 		assert_equal(true,teacher_input_array(student_name).include?("jeremy"))
+# 	end
+
+# 	def test_assert_that_teacher_input_number_is_student_array_number
+# 		student_name = nil
+# 		assert_equal(4, teacher_input_array(student_name).count)
+# 	end
+# end
+
 require "minitest/autorun"
 require_relative "random_pair.rb"
 
@@ -8,20 +33,12 @@ class TestRandom < Minitest::Test
 	end
 
 	def test_assert_that_there_is_an_array
-		student_name = nil
-		student_number = 0
-		assert_equal([],teacher_input_array(student_name, student_number))
+		student_name = ""
+		assert_equal([],student_list(student_name))
 	end
 
 	def test_assert_that_student_array_returns_correct_value
 		student_name = "jeremy"
-		student_number = 1
-		assert_equal(true,teacher_input_array(student_name, student_number).include?("jeremy"))
-	end
-
-	def test_assert_that_teacher_input_number_is_student_array_number
-		student_name = nil
-		student_number = 4
-		assert_equal(4, teacher_input_array(student_name, student_number).count)
+		assert_equal(["jeremy"],student_list(student_name))
 	end
 end
