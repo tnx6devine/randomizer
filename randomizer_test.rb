ab706d8790b762d56e648b2ec3ec95630e508bae
triciahoover <tricia.l.hoover@gmail.com>
2017-08-08 09:31:03 -0400

    1st tests that failed

--- randomizer_test.rb
+++ randomizer_test.rb
@@ -6,4 +6,8 @@ class TestRandom < Minitest::Test
 	def test_assert_that_1_equals_1
 		assert_equal(1,1)
 	end
+
+	def test_assert_that_there_is_an_array
+		assert_equal([],student_list())
+	end
 end
