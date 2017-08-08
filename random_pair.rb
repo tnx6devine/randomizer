e1daaa7814138fef2c7230569e652f2fc6782fc0
Trixie Devine <sward@Stacy-Wards-MacBook-Pro-3.local>
2017-08-08 11:29:10 -0400

    cleaned up comments

--- random_pair.rb
+++ random_pair.rb
@@ -1,28 +1,18 @@
 
 
-# def student_list(student_name)
-# 	if student_name != ""
-# 		$student_array.push(student_name)
-# 	end
-# 	$student_array
-# end
-
-
 def teacher_input_array(student_name, student_number)
-	$student_array = []
-	# puts $student_array
+	student_array = []
 	if student_name == nil
 		ask = student_number.to_i
 		ask.times do
 		p "What is student's name?"
 		teacher_input = gets.chomp
-		$student_array.push(teacher_input)
+		student_array.push(teacher_input)
 		end
 	else 
-		$student_array.push(student_name)
+		student_array.push(student_name)
 	end
-	$student_array.to_a
-	# $student_array.length
+	student_array.to_a
 end
 
 # student_name = nil
